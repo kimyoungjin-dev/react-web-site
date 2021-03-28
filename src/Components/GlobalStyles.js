e75cd729f1e50dd8e869634ccb950d1b2b8ffdff
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-29 02:53:16 +0900

    reset css

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+
+    *{
+        box-sizing:border-box;
+        padding:0;
+        margin:0;
+    }
+
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
+
+export default globalStyles;
