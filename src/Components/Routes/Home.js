4728e2338f656094f5153fdb3fe357c7d204bb62
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-30 00:22:41 +0900

    Compose the logic by making the screen showing the video into a component.

--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -1,46 +1,20 @@
 import React from "react";
-import BackgroundVideos from "videos/video.mp4";
 import styled from "styled-components";
-import CenterTitle from "Components/CenterTitle";
+import Video from "Components/Home/Video";
+import Travel from "Components/Home/Travel";
+import Community from "Components/Home/Bottom/Community";
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
 `;
 
-const VideoContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const VideosBox = styled.video`
-  object-fit: cover;
-  -o-object-fit: cover;
-  background-color: #232a34;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-`;
-
 const Home = () => {
   return (
     <Container>
-      <VideoContainer>
-        <VideosBox
-          src={BackgroundVideos}
-          autoPlay
-          muted
-          loop
-          type="video/mp4"
-        />
-        <CenterTitle />
-      </VideoContainer>
-      <div>hello</div>
+      <Video />
+      <Travel />
+      <Community />
     </Container>
   );
 };
